4cbb912b9e27146701d2b29a4fded3a20e6fdf25
diogolimas <diogo.libras43@gmail.com>
2021-12-15 11:01:28 -0300

    feat: first step, alocating OCP concepts example class to Open-Closed Principle.

---
+++ Ocp/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ocp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
---
+++ Ocp/Vehicle.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ocp
+{
+    public class Vehicle
+    {
+        private string color;
+        private int year;
+        private double engine;
+        private int seats;
+        private int doors;
+
+        public Vehicle(string color, int year, double engine, int seats, int doors)
+        {
+            this.color = color;
+            this.year = year;
+            this.engine = engine;
+            this.seats = seats;
+            this.doors = doors;
+        }
+    }
+}
